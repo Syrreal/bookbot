df8723dfb894627f1b260eb5dbf430f558624aa3
Syrreal <SDHeil4561@gmail.com>
2025-03-31 19:53:46 -0700

    updated main to use new stats method

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from stats import *
 
 def get_book_text(path):
     with open(path) as f:
@@ -6,7 +7,11 @@ def get_book_text(path):
 
 def main():
     book = "./books/frankenstein.txt"
-    print(get_book_text(book))
+    text = get_book_text(book)
+    num_words = total_words(text)
+    num_letters = letter_count(text)
+    print(f'{num_words} words found in the document')
+    print(num_letters)
 
 if __name__ == "__main__":
     main()
