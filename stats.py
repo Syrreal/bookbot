0d343b41a0ffe4c1a325e92aa45b28f0b7e25e2c
Syrreal <SDHeil4561@gmail.com>
2025-03-31 19:53:27 -0700

    Added new method to stats

--- stats.py
+++ stats.py
@@ -2,3 +2,14 @@
 def total_words(text):
     num_words = len(text.split())
     return num_words
+
+def letter_count(text):
+    lower_text = text.lower()
+    letter_dict = {}
+    for letter in lower_text:
+
+        if letter in letter_dict:
+            letter_dict[letter] += 1
+        else:
+            letter_dict[letter] = 1
+    return letter_dict
