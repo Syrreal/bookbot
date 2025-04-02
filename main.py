9832bdc3338ff4cb18b3569204aaa99662b1fed8
Syrreal <SDHeil4561@gmail.com>
2025-04-01 23:23:48 -0700

    Added pretty print and adjusted output text

--- main.py
+++ main.py
@@ -4,14 +4,23 @@ def get_book_text(path):
     with open(path) as f:
         text = f.read()
         return text
+    
+def pretty_print_characters(sorted_letters):
+    print("-------- Character Count --------")
+    for set in sorted_letters:
+        print(f'{set[0]}: {set[1]}')
 
 def main():
+    print("========== BOOKBOT ==========")
     book = "./books/frankenstein.txt"
+    print(f'Analyzing book found at {book}...')
     text = get_book_text(book)
     num_words = total_words(text)
+    print('---------- Word Count ----------\n'
+           f'Found {num_words} total words')
     num_letters = letter_count(text)
-    print(f'{num_words} words found in the document')
-    print(num_letters)
-
+    sorted_letters = sort_dict(num_letters)
+    pretty_print_characters(sorted_letters)
+    print("========== END ==========")
 if __name__ == "__main__":
     main()
