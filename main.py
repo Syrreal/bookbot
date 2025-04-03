36cfbfede22bdb1266a0630679efe7ae80c7dc3e
Syrreal <SDHeil4561@gmail.com>
2025-04-02 21:38:09 -0700

    Updated to use user supplied path rather than hardcoded path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import *
+import sys
 
 def get_book_text(path):
     with open(path) as f:
@@ -6,21 +7,30 @@ def get_book_text(path):
         return text
     
 def pretty_print_characters(sorted_letters):
-    print("-------- Character Count --------")
     for set in sorted_letters:
         print(f'{set[0]}: {set[1]}')
 
-def main():
+def main(path):
+    book = path
     print("========== BOOKBOT ==========")
-    book = "./books/frankenstein.txt"
     print(f'Analyzing book found at {book}...')
+    print('---------- Word Count ----------')
+
     text = get_book_text(book)
     num_words = total_words(text)
-    print('---------- Word Count ----------\n'
-           f'Found {num_words} total words')
+    print(f'Found {num_words} total words')
+
     num_letters = letter_count(text)
     sorted_letters = sort_dict(num_letters)
+    
+    print("-------- Character Count --------")
     pretty_print_characters(sorted_letters)
     print("========== END ==========")
+
 if __name__ == "__main__":
-    main()
+    args = sys.argv
+    if len(args) > 1:
+        main(args[1])
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
